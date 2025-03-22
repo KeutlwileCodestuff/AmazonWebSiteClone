3e688a6cfcfe2b94b5e39fe0d9bf8a4413e5ace0
WeThinkCode Student <keutlwilempolokeng@gmail.com>
2025-03-22 18:05:08 +0200

    checkout products displays

--- cart/cart.js
+++ cart/cart.js
@@ -1,1 +1,14 @@
-export const cart = [];
+export const cart = [
+
+    {
+        id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
+        name: "Black and Gray Athletic Cotton Socks - 6 Pairs",
+        quantity:2,
+
+      },
+      {
+        id: "15b6fc6f-327a-4ec4-896f-486349e85a3d",
+        name: "Intermediate Size Basketball",
+        quantity:3
+      }
+];
--- checkout/checkout.js
+++ checkout/checkout.js
@@ -1,94 +1,93 @@
-import { cart } from "../cart/cart";
-import { products } from "../data/products";
-
+import { cart } from "../cart/cart.js";
+import { products } from "../data/products.js";
 let cartHtml = '';
-
-
-cart.forEach((item) =>{
-    let machingItem;
-    products.forEach((product) =>{
+cart.forEach((item) => {
+    let MatchingItem;
+    products.forEach((product) => {
         if(item.id === product.id){
-            machingItem = product;
+            MatchingItem = product;
         };
     });
 
-    console.log(machingItem);
-
-
-    cartHtml +=  `
-    <div class="cart-item-container">
-        <div class="delivery-date">
-        Delivery date: Wednesday, June 15
+    cartHtml += `<div class="cart-item-container">
+    <div class="delivery-date">
+      Delivery date: Tuesday, June 21
+    </div>
+    
+    <div class="cart-item-details-grid">
+      <img class="product-image"
+        src="${MatchingItem.image}">
+    
+      <div class="cart-item-details">
+        <div class="product-name">
+          Black and Gray Athletic Cotton Socks - 6 Pairs
         </div>
-        <div class="cart-item-details-grid">
-        <img class="product-image"
-            src=" ${machingItem.image} ">
-
-        <div class="cart-item-details">
-            <div class="product-name">
-            ${machingItem.name}
-            </div>
-            <div class="product-price">
-            ${machingItem.price}
-            </div>
-            <div class="product-quantity">
-            <span>
-                Quantity: <span class="quantity-label">${machingItem.quantity}</span>
-            </span>
-            <span class="update-quantity-link link-primary">
-                Update
-            </span>
-            <span class="delete-quantity-link link-primary">
-                Delete
-            </span>
-            </div>
+        <div class="product-price">
+          $${(MatchingItem.priceCents / 100).toFixed(2)}
         </div>
-
-    <div class="delivery-options">
+        <div class="product-quantity">
+          <span>
+            Quantity: <span class="quantity-label">${item.quantity}</span>
+          </span>
+          <span class="update-quantity-link link-primary">
+            Update
+          </span>
+          <span class="delete-quantity-link link-primary">
+            Delete
+          </span>
+        </div>
+      </div>
+    
+      <div class="delivery-options">
         <div class="delivery-options-title">
-        Choose a delivery option:
-        </div>
-
-        <div class="delivery-option">
-        <input type="radio" class="delivery-option-input"
-            name="delivery-option-2">
-        <div>
-            <div class="delivery-option-date">
-            Tuesday, June 21
-            </div>
-            <div class="delivery-option-price">
-            FREE Shipping
-            </div>
-        </div>
+          Choose a delivery option:
         </div>
         <div class="delivery-option">
-        <input type="radio" checked class="delivery-option-input"
-            name="delivery-option-2">
-        <div>
+          <input type="radio" checked
+            class="delivery-option-input"
+            name="delivery-option-1">
+          <div>
             <div class="delivery-option-date">
-            Wednesday, June 15
+              Tuesday, June 21
             </div>
             <div class="delivery-option-price">
-            $4.99 - Shipping
+              FREE Shipping
             </div>
-        </div>
+          </div>
         </div>
         <div class="delivery-option">
-        <input type="radio" class="delivery-option-input"
-            name="delivery-option-2">
-        <div>
+          <input type="radio"
+            class="delivery-option-input"
+            name="delivery-option-1">
+          <div>
             <div class="delivery-option-date">
-            Monday, June 13
+              Wednesday, June 15
             </div>
             <div class="delivery-option-price">
-            $9.99 - Shipping
+              $4.99 - Shipping
             </div>
+          </div>
         </div>
+        <div class="delivery-option">
+          <input type="radio"
+            class="delivery-option-input"
+            name="delivery-option-1">
+          <div>
+            <div class="delivery-option-date">
+              Monday, June 13
+            </div>
+            
+            <div class="delivery-option-price">
+              $9.99 - Shipping
+            </div>
+          </div>
         </div>
+      </div>
     </div>
     </div>
-    </div>   
     `;
 });
-
 document.querySelector('.order-summary').innerHTML = cartHtml;
+
+
+
