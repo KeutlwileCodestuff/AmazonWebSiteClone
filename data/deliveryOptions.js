50404ed1116b526d2b72a8993fcafdc4793bea2e
WeThinkCode Student <keutlwilempolokeng@gmail.com>
2025-03-27 21:21:36 +0200

    normalizng data

---
+++ data/deliveryOptions.js
@@ -0,0 +1,19 @@
+import { deliveryDates } from "../scripts/utils/time";
+
+const DeliveryOptions = [
+    {
+        id: '1',
+        deliveryDays: 7,
+        priceCents:0
+    },
+    {
+        id:'2',
+        deliveryDays: 3,
+        priceCents:499,
+    },
+    {
+        id:'3',
+        deliveryDates:0,
+        priceCents:999
+    }
+]
